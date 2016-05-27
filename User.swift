03c3be6992cd48a71e07db80bf94d47acdd4596a
[01;31m[K"name"[m[K: "Wyeth Jackson <[01;31m[K"email"[m[K: "wyethjackson@gmail.com>
2016-05-27 07:49:27 -0500

    merge

--- User.swift
+++ User.swift
@@ -15,3 +15,28 @@
 //    var business = String()
 //    var companyName = String()
 //}
+
+//import Foundation
+//import CoreLocation
+//import Parse
+//
+//import Foundation
+//
+//class User : PFUser {
+//    
+////    override class func load() {
+////        self.registerSubclass()
+////    }
+////    func parseClassName() -> String! {
+////        return "User"
+////    
+////    }
+//    //My variables
+//    dynamic var firstName: String = ""
+//    dynamic var lastName: String = ""
+//    dynamic var business: String = ""
+//    dynamic var homeCountry: String = ""
+//    dynamic var fullName: String = ""
+//    
+//    
+//}
